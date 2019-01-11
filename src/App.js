f998c883d01e02326c7b83ed1c37ecbdcbaa2c86
GabrielTopciu <lostglory15@gmail.com>
2019-01-11 08:24:09 +0000

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import  './App.css'
 
 
 class App extends Component {
-    constructor() {
+    constructor()   {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
     }
     onSearchChange = (event) => {
     this.setState({searchfield:event.target.value})
@@ -21,6 +26,11 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter (robots => { 
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+
+        if(this.state.robots.length === 0 ) {
+            return <h1>Loading</h1>
+        }
+        else{
     return (
         <div className = 'tc'>
             <h1 className= 'f1'>Robofriends</h1>
@@ -30,5 +40,7 @@ class App extends Component {
     );
 }
 }
+}
+
 
 export default App;
